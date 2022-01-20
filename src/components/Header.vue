37b666e65e79d0f4e65b3c7175df41fefeb2177f
Rodrigo <rodluna97@gmail.com>
2022-01-20 18:27:52 -0300

    nova navbar mas falta correcao

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,18 @@
 <template>
     
-    <div class="container navbar-fixed-top navbar-inverse navbar-transparente">
-    <header class="d-flex  justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto  text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-        <span class="img-logo">Spotify</span> 
-      </a>
-      
+    <div class="container">
+
+    <nav class="navbar navbar-expand-lg navbar-transparente navbar-fixed-top ">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto  text-decoration-none">
+          <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+          <span class="img-logo">Spotify</span> 
+        </a>
+    
       <ul class="nav">
         <li class="nav-item"><a href="#" class="nav-link" aria-current="page">Premium</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Suporte</a></li>
@@ -14,9 +20,10 @@
         <li class="nav-item"><a href="#" class="nav-link">|</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Inscrever-se</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Entrar</a></li>
-      </ul>
-    </header>
-  </div>
+      </ul>"
+    
+     </nav>
+    </div>
     
 </template>
 
@@ -48,5 +55,14 @@ h1{
 	font-weight: 900;
 	letter-spacing: -0.04px;
 	margin-bottom: 50px;
+}
+nav.navbar-transparente{
+	padding: 15px 0px;
+	background: rgba(0,0,0, 0.6);
+	border: none;
+  
+}
+.navbar-brand{
+	padding: 5px 15px;
 }
 </style>
